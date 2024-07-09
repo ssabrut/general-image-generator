96e08dbd564a5d74553b5f59e8747f4af8589972
ssabrut <michael.gunawan2002@gmail.com>
2024-07-09 11:24:27 +0700

    Add generate image logic scaffolding

--- app.py
+++ app.py
@@ -7,6 +7,22 @@ from PIL import Image
 from dotenv import load_dotenv
 load_dotenv()
 
+# initialize client
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+def generate_images(description: str, num_of_images: int):
+    img_response = client.images.generate(
+        model="dall-e-3"
+        prompt=description,
+        size="1024x1024"
+        quality="standard"
+        n=1
+    )
+
+    img_url = img_response.data[0].url
+    return img_url
+
+# set page config
 st.set_page_config(page_title="DALL-E Image Generator", page_icon=":camera:")
 
 # heading
@@ -19,4 +35,4 @@ num_of_images = st.slider("Number of images to generate", 1, 5, 1)
 generate_btn = st.button("Generate Images")
 
 if generate_btn:
-    pass
+    generated_img = generate_images(img_description, num_of_images)
