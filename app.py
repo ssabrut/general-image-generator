5551a4877ba130c9085065cff67b4f82616f0471
ssabrut <michael.gunawan2002@gmail.com>
2024-07-09 11:21:17 +0700

    UI scaffolding

--- app.py
+++ app.py
@@ -14,4 +14,9 @@ st.title("DALL-E Image Generator")
 st.subheader("Powered by OpenAI's DALL-E")
 
 # body
-img_description = st.text_input("Enter description for the image")
+img_description = st.text_input("Enter description for the image you want to generate")
+num_of_images = st.slider("Number of images to generate", 1, 5, 1)
+generate_btn = st.button("Generate Images")
+
+if generate_btn:
+    pass
